1ed15385ccba91bfd778d25c0d955c0aee8385a6
Becky McCrea <rebecca.mccrea@iowa.gov>
2018-01-11 09:19:49 -0600

    configuring for azure

--- app/app.config.js
+++ app/app.config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var AppConfig = /** @class */ (function () {
     function AppConfig() {
-        this.apiUrl = 'http://localhost:59822';
+        this.apiUrl = 'https://testangularwebapi.azurewebsites.net';
     }
     return AppConfig;
 }());
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,9 @@
-﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+﻿import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+enableProdMode();
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.log(err));
