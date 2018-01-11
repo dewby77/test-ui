f4dbbd4a7b132c669b651293022d5e09d2361309
Becky McCrea <rebecca.mccrea@iowa.gov>
2018-01-10 20:47:40 -0600

    update for local

--- app/app.config.js
+++ app/app.config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var AppConfig = /** @class */ (function () {
     function AppConfig() {
-        this.apiUrl = 'https://testangularwebapi.azurewebsites.net';
+        this.apiUrl = 'http://localhost:59822';
     }
     return AppConfig;
 }());
--- app/app.config.ts
+++ app/app.config.ts
@@ -1,3 +1,3 @@
 ﻿export class AppConfig {
-    public readonly apiUrl = 'https://testangularwebapi.azurewebsites.net';
+    public readonly apiUrl = 'http://localhost:59822';
 };
